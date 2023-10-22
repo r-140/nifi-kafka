9f1e5a703b2a2ef91f23dec13061c1538722a6ed
Ilya Ushakov <ilya.ushakov@globallogic.com>
2023-10-22 15:37:04 +0200

    kafka consumer was implemented

---
+++ src/main/java/BitStampKafkaConsumer.java
@@ -0,0 +1,47 @@
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.log4j.Logger;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Properties;
+
+public class BitStampKafkaConsumer {
+    final static Logger log = Logger.getLogger(BitStampKafkaConsumer.class);
+    public static void main(String[] args) {
+        log.info("starting consuming massages");
+
+        String bootstrapServers = "127.0.0.1:9092";
+        String groupId = "bitstamp-trn";
+        String topic = "bitstamp-trn-topic";
+
+        // create consumer configs
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        // create consumer
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+
+        consumer.subscribe(Collections.singletonList(topic));
+
+        // poll for new data
+        while(true){
+            ConsumerRecords<String, String> records =
+                    consumer.poll(Duration.ofMillis(100));
+
+            for (ConsumerRecord<String, String> record : records){
+                log.info("Key: " + record.key() + ", Value: " + record.value());
+                log.info("Partition: " + record.partition() + ", Offset:" + record.offset());
+            }
+        }
+
+    }
+}
