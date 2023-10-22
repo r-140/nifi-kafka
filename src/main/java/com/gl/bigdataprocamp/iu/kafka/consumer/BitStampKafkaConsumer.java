dc2b1af89e0f9a7d4b80aea36da507bb63954ad2
Ilya Ushakov <ilya.ushakov@globallogic.com>
2023-10-22 16:17:57 +0200

    kafka consumer was implemented

--- src/main/java/com/gl/bigdataprocamp/iu/kafka/consumer/BitStampKafkaConsumer.java
+++ src/main/java/com/gl/bigdataprocamp/iu/kafka/consumer/BitStampKafkaConsumer.java
@@ -5,20 +5,20 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.log4j.Logger;
 
 import java.time.Duration;
 import java.util.Collections;
 import java.util.Properties;
+import java.util.logging.Logger;
 
 public class BitStampKafkaConsumer {
-    final static Logger log = Logger.getLogger(BitStampKafkaConsumer.class);
+    final static Logger log = Logger.getLogger("BitStampKafkaConsumer");
     public static void main(String[] args) {
         log.info("starting consuming massages");
-        System.out.println("****starring consuming****");
+        System.out.println("****starting consuming****");
 
         String bootstrapServers = "127.0.0.1:9092";
-        String groupId = "bitstamp-trn";
+        String groupId = "bitstamp-trn-group";
         String topic = "bitstamp-trn-topic";
 
         // create consumer configs
@@ -26,7 +26,7 @@ public class BitStampKafkaConsumer {
         properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-//        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
         // create consumer
@@ -38,6 +38,8 @@ public class BitStampKafkaConsumer {
         while(true){
             ConsumerRecords<String, String> records =
                     consumer.poll(Duration.ofMillis(100));
+
+            System.out.println("number of records " + records.count());
 
             for (ConsumerRecord<String, String> record : records){
                 log.info("Key: " + record.key() + ", Value: " + record.value());
