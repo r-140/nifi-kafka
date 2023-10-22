7ed679b3335775c95887a3d20029986021eba296
Ilya Ushakov <ilya.ushakov@globallogic.com>
2023-10-22 16:20:49 +0200

    kafka consumer was implemented

--- src/main/java/com/gl/bigdataprocamp/iu/kafka/consumer/BitStampKafkaConsumer.java
+++ src/main/java/com/gl/bigdataprocamp/iu/kafka/consumer/BitStampKafkaConsumer.java
@@ -15,7 +15,6 @@ public class BitStampKafkaConsumer {
     final static Logger log = Logger.getLogger("BitStampKafkaConsumer");
     public static void main(String[] args) {
         log.info("starting consuming massages");
-        System.out.println("****starting consuming****");
 
         String bootstrapServers = "127.0.0.1:9092";
         String groupId = "bitstamp-trn-group";
@@ -39,13 +38,9 @@ public class BitStampKafkaConsumer {
             ConsumerRecords<String, String> records =
                     consumer.poll(Duration.ofMillis(100));
 
-            System.out.println("number of records " + records.count());
-
             for (ConsumerRecord<String, String> record : records){
                 log.info("Key: " + record.key() + ", Value: " + record.value());
-                System.out.println("Key: " + record.key() + ", Value: " + record.value());
                 log.info("Partition: " + record.partition() + ", Offset:" + record.offset());
-                System.out.println("Partition: " + record.partition() + ", Offset:" + record.offset());
             }
         }
 
