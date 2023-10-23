fde35f1e646c9d1845f728c9489e1c0a4bf023a3
Ilya Ushakov <ilya.ushakov@globallogic.com>
2023-10-23 16:00:56 +0200

    NPE in comparator fixed

--- src/main/java/com/gl/bigdataprocamp/iu/kafka/consumer/util/BitStampsProcessedStorage.java
+++ src/main/java/com/gl/bigdataprocamp/iu/kafka/consumer/util/BitStampsProcessedStorage.java
@@ -1,16 +1,26 @@
 package com.gl.bigdataprocamp.iu.kafka.consumer.util;
 
-import com.gl.bigdataprocamp.iu.kafka.consumer.comparator.BitStampTrnByPriceComparator;
 import com.gl.bigdataprocamp.iu.kafka.consumer.dto.BitStampTrn;
 
+import java.util.Comparator;
 import java.util.TreeSet;
 
 public enum BitStampsProcessedStorage {
     INSTANCE;
     private static final int MAX_RESULT = 10;
 
+    private final Comparator<BitStampTrn> PRICE_COMPARATOR =
+            Comparator.comparing(p->
+                {
+                    if (p.getData() != null && p.getData().getPrice() != null) {
+                        return p.getData().getPrice();
+                    } else {
+                        return Double.valueOf(0);
+                    }
+                }
+            );
     private TreeSet<BitStampTrn> bitStampTrns =
-            new TreeSet<>((new BitStampTrnByPriceComparator()).reversed());
+            new TreeSet<>(PRICE_COMPARATOR.reversed());
 
     public TreeSet<BitStampTrn> getBitStampTrns() {
         return bitStampTrns;
